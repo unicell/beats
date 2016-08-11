d64656d05aecca885289196b07bf96a27818a647
Qiu Yu <unicell@gmail.com>
2016-08-10 17:33:49 -0700

    buildable

---
+++ swiftbeat/beater/swiftbeat.go
@@ -0,0 +1,119 @@
+package beater
+
+import (
+	"fmt"
+
+	"github.com/elastic/beats/libbeat/beat"
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/logp"
+	cfg "github.com/elastic/beats/swiftbeat/config"
+	"github.com/elastic/beats/swiftbeat/crawler"
+	//"github.com/elastic/beats/filebeat/input"
+	//"github.com/elastic/beats/filebeat/publish"
+	//"github.com/elastic/beats/filebeat/registrar"
+	//"github.com/elastic/beats/filebeat/spooler"
+)
+
+// Swiftbeat is a beater object. Contains all objects needed to run the beat
+type Swiftbeat struct {
+	config *cfg.Config
+	done   chan struct{}
+}
+
+// New creates a new Swiftbeat pointer instance.
+func New(b *beat.Beat, rawConfig *common.Config) (beat.Beater, error) {
+	config := cfg.DefaultConfig
+	if err := rawConfig.Unpack(&config); err != nil {
+		return nil, fmt.Errorf("Error reading config file: %v", err)
+	}
+	if err := config.FetchConfigs(); err != nil {
+		return nil, err
+	}
+
+	sb := &Swiftbeat{
+		done:   make(chan struct{}),
+		config: &config,
+	}
+	return sb, nil
+}
+
+// Run allows the beater to be run as a beat.
+func (sb *Swiftbeat) Run(b *beat.Beat) error {
+	//var err error
+	config := sb.config
+
+	// Setup registrar to persist state
+	//registrar, err := registrar.New(config.RegistryFile)
+	//if err != nil {
+	//logp.Err("Could not init registrar: %v", err)
+	//return err
+	//}
+
+	// Channel from harvesters to spooler
+	//publisherChan := make(chan []*input.Event, 1)
+
+	// Publishes event to output
+	//publisher := publish.New(config.PublishAsync,
+	//publisherChan, registrar.Channel, b.Publisher)
+
+	// Init and Start spooler: Harvesters dump events into the spooler.
+	//spooler, err := spooler.New(config, publisherChan)
+	//if err != nil {
+	//logp.Err("Could not init spooler: %v", err)
+	//return err
+	//}
+
+	//logp.Err("Could not init crawler: %v", config.Prospectors)
+	//logp.Debug("prospector", "File Configs: %v", config.Prospectors[0].config.Paths)
+	// TODO //
+	crawler, err := crawler.New(config.Prospectors)
+	//crawler, err := crawler.New(spooler, config.Prospectors)
+	if err != nil {
+		logp.Err("Could not init crawler: %v", err)
+		return err
+	}
+
+	// The order of starting and stopping is important. Stopping is inverted to the starting order.
+	// The current order is: registrar, publisher, spooler, crawler
+	// That means, crawler is stopped first.
+
+	// Start the registrar
+	//err = registrar.Start()
+	//if err != nil {
+	//logp.Err("Could not start registrar: %v", err)
+	//}
+	// Stopping registrar will write last state
+	//defer registrar.Stop()
+
+	// Start publisher
+	//publisher.Start()
+	// Stopping publisher (might potentially drop items)
+	//defer publisher.Stop()
+
+	// Starting spooler
+	//spooler.Start()
+	// Stopping spooler will flush items
+	//defer spooler.Stop()
+
+	// TODO //
+	//err = crawler.Start(registrar.GetStates())
+	err = crawler.Start()
+	if err != nil {
+		return err
+	}
+	// Stop crawler -> stop prospectors -> stop harvesters
+	defer crawler.Stop()
+
+	// Blocks progressing. As soon as channel is closed, all defer statements come into play
+	<-sb.done
+
+	return nil
+}
+
+// Stop is called on exit to stop the crawling, spooling and registration processes.
+func (sb *Swiftbeat) Stop() {
+	logp.Info("Stopping swiftbeat")
+
+	// Stop Swiftbeat
+	close(sb.done)
+}
---
+++ swiftbeat/crawler/crawler.go
@@ -0,0 +1,86 @@
+package crawler
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/logp"
+	//"github.com/elastic/beats/swiftbeat/input/file"
+	//"github.com/elastic/beats/swiftbeat/prospector"
+	//"github.com/elastic/beats/swiftbeat/spooler"
+)
+
+type Crawler struct {
+	//prospectors       []*prospector.Prospector
+	prospectorConfigs []*common.Config
+	//spooler           *spooler.Spooler
+	wg sync.WaitGroup
+}
+
+// TODO //
+//func New(spooler *spooler.Spooler, prospectorConfigs []*common.Config) (*Crawler, error) {
+func New(prospectorConfigs []*common.Config) (*Crawler, error) {
+
+	if len(prospectorConfigs) == 0 {
+		return nil, fmt.Errorf("No prospectors defined. You must have at least one prospector defined in the config file.")
+	}
+
+	return &Crawler{
+		//spooler:           spooler,
+		prospectorConfigs: prospectorConfigs,
+	}, nil
+}
+
+// TODO //
+//func (c *Crawler) Start(states file.States) error {
+func (c *Crawler) Start() error {
+
+	logp.Info("Loading Prospectors: %v", len(c.prospectorConfigs))
+
+	// Prospect the globs/paths given on the command line and launch harvesters
+	//for _, prospectorConfig := range c.prospectorConfigs {
+
+	//prospector, err := prospector.NewProspector(prospectorConfig, states, c.spooler.Channel)
+	//if err != nil {
+	//return fmt.Errorf("Error in initing prospector: %s", err)
+	//}
+	//c.prospectors = append(c.prospectors, prospector)
+	//}
+
+	//logp.Info("Loading Prospectors completed. Number of prospectors: %v", len(c.prospectors))
+
+	//for i, p := range c.prospectors {
+	//c.wg.Add(1)
+
+	//go func(id int, prospector *prospector.Prospector) {
+	//defer func() {
+	//c.wg.Done()
+	//logp.Debug("crawler", "Prospector %v stopped", id)
+	//}()
+	//logp.Debug("crawler", "Starting prospector %v", id)
+	//prospector.Run()
+	//}(i, p)
+	//}
+
+	//logp.Info("All prospectors are initialised and running with %d states to persist", states.Count())
+
+	return nil
+}
+
+func (c *Crawler) Stop() {
+	logp.Info("Stopping Crawler")
+	//stopProspector := func(p *prospector.Prospector) {
+	//defer c.wg.Done()
+	//p.Stop()
+	//}
+
+	//logp.Info("Stopping %v prospectors", len(c.prospectors))
+	//for _, p := range c.prospectors {
+	// Stop prospectors in parallel
+	//c.wg.Add(1)
+	//go stopProspector(p)
+	//}
+	c.wg.Wait()
+	logp.Info("Crawler stopped")
+}
